aeadfd10e8a9e5c4ef0cf709cdb0248844d1a929
18-proj <67160300+18-proj@users.noreply.github.com>
2020-06-20 03:31:31 +0900

    Add files via upload

--- 100_calc/charec.js
+++ 100_calc/charec.js
@@ -165,6 +165,7 @@ Charlec.prototype = {
             window.addEventListener('touchmove', function(event) {
               event.preventDefault();
             },false);
+	        this.$field.addEventListener('touchmove', scrollControll, {passive: false});
 	        this.$field.addEventListener('touchstart', this.onTouchstart.bind(this));
 	        this.$field.addEventListener('touchmove', this.onTouchmove.bind(this));
 	        this.$field.addEventListener('touchend', this.onTouchend.bind(this));
